fec4fd50ca5e8a722599c91da71492b43c61d9ae
Maksym Hryshchenko <35242142+Makillaa@users.noreply.github.com>
2021-09-04 16:44:22 +0300

    Update apllication.py

--- apllication.py
+++ apllication.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-import os
-import hello
+# import hello
+
 application = Flask(__name__)
 
-@application.route("/hello")
+@application.route("/")
 def hellopage():
-    return hello
+    return "Hi"
   
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=8080)
